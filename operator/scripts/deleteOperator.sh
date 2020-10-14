3276891359114bf60f93fb0e77e412e4019a7ed6
swapnil280493 <67164160+swapnil280493@users.noreply.github.com>
2020-10-14 11:25:22 +0530

    Add files via upload

--- operator/scripts/deleteOperator.sh
+++ operator/scripts/deleteOperator.sh
@@ -3,16 +3,19 @@
 #
 # Licensed Materials - Property of IBM
 #
-# (C) Copyright IBM Corp. 2019. All Rights Reserved.
+# (C) Copyright IBM Corp. 2020. All Rights Reserved.
 #
 # US Government Users Restricted Rights - Use, duplication or
 # disclosure restricted by GSA ADP Schedule Contract with IBM Corp.
 #
 ###############################################################################
-kubectl delete -f descriptors/operator.yaml
-kubectl delete -f descriptors/role_binding.yaml
-kubectl delete -f descriptors/role.yaml
-kubectl delete -f descriptors/service_account.yaml
+# CUR_DIR set to full path to scripts folder
+CUR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-kubectl patch crd/ierclusters.ier.ibm.com -p '{"metadata":{"finalizers":[]}}' --type=merge
-kubectl delete crd ierclusters.ier.ibm.com
+kubectl delete -f ${CUR_DIR}/../descriptors/operator.yaml
+kubectl delete -f ${CUR_DIR}/../descriptors/role_binding.yaml
+kubectl delete -f ${CUR_DIR}/../descriptors/role.yaml
+kubectl delete -f ${CUR_DIR}/../descriptors/service_account.yaml
+
+
+echo "All descriptors have been successfully deleted."
